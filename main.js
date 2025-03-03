f8666eddbd11fdc188f783280bbe1ce83bf88d21
nabaraj basnet <nabarajbasnet2000@gmail.com>
2025-03-03 08:04:11 +0545

    Final commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fetchActivities = require("./utils/response");
 const username = process.argv[2];
 
 if (!username) {
-    throw new Error('Username not available!');
+    throw new Error('Github username not available!');
 };
 
 const renderActivities = (events) => {
@@ -11,19 +11,28 @@ const renderActivities = (events) => {
     };
 
     events.forEach(event => {
+        let activity = null;
         switch (event.type) {
             case 'PushEvent':
-                const commitLength = event.payload.commits.length
-                console.log(commitLength);
-                const activity = `Pushed ${commitLength} commits to ${event.repo.name}`
-                break
+                const commitLength = event.payload.commits.length;
+                activity = `Pushed ${commitLength} commits to ${event.repo.name}`;
+                break;
+
+            case 'IssuesEvent':
+                activity = `Opened a new issue in ${event.repo.name}`;
+                break;
+
+            case 'WatchEvent':
+                activity = `Starred ${event.repo.name}`;
+                break;
 
             default:
                 break;
         }
+        console.log('Activity: ', activity);
     });
 }
 
-fetchActivities(username)
+fetchActivities('kamranahmedse')
     .then((events) => renderActivities(events))
     .catch(err => console.log(err))
